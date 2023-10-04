a5e2f8fea605b0b3f6941845001d8ece8134dab3
Rajeeradha <127470577+Rajeeradha@users.noreply.github.com>
2023-10-05 00:12:30 +1100

    Add files via upload

---
+++ src/component/imgdata.js
@@ -0,0 +1,80 @@
+import Cardbanner1 from '../img/Cardbanner1.jpeg';
+import Cardbanner2 from '../img/Cardbanner2.jpg';
+import Cardbanner3 from '../img/Cardbanner3.png';
+import Cardbanner4 from '../img/Cardbanner4.jpeg';
+import Cardbanner5 from '../img/Cardbanner5.jpeg';
+import Cardbanner6 from '../img/Cardbanner6.jpeg';
+import Cardbanner7 from '../img/Cardbanner7.jpeg';
+import Cardbanner8 from '../img/Cardbanner8.jpeg';
+
+
+export const imgdata = [ Cardbanner1,Cardbanner1,
+    Cardbanner2,Cardbanner3,Cardbanner4,Cardbanner5,
+    Cardbanner6,Cardbanner7,Cardbanner8];
+
+export const data = [
+    {   
+        id:1,
+        name:'Fancy Product',
+        price:"$150",
+        discount:'$120',
+        available:'sold',
+        added:true
+    },
+    {   
+        id:2,
+        name:'Fancy Product',
+        price:"$45",
+        discount:'$25',
+        available:'',
+        added:true
+    }, 
+    {   
+        id:3,
+        name:'Fancy Product',
+        price:'$35',
+        discount:'$25',
+        available:'sold',
+        added:true
+    },
+    {   
+        id:4,
+        name:'Fancy Product',
+        price:"$45",
+        discount:'$35',
+        available:'',
+        added:true
+    },    
+    {   
+        id:5,
+        name:'Fancy Product',
+        price:"$25",
+        discount:'$20',
+        available:'',
+        added:true
+    },
+    {   
+        id:6,
+        name:'Fancy Product',
+        price:"$65",
+        discount:'$55',
+        available:'sold',
+        added:true
+    },   
+    {   
+        id:7,
+        name:'Fancy Product',
+        price:"$40",
+        discount:'$30',
+        available:'',
+        added:true
+    },
+    {   
+        id:8,
+        name:'Fancy Product',
+        price:"$45",
+        discount:'$25',
+        available:'',
+        added:true
+    }
+    ]
